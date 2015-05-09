9e991884ef800924ea0d1cdb61eff48ef2fd5010
Thaddeus Ternes <thaddeus@bluetoo.co>
2015-05-09 16:49:51 -0500

    Initial Commit

---
+++ BTCron/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BTCron
+//
+//  Created by Thaddeus Ternes on 5/9/15.
+//  Copyright (c) 2015 Bluetoo Ventures. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ BTCron/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  BTCron
+//
+//  Created by Thaddeus Ternes on 5/9/15.
+//  Copyright (c) 2015 Bluetoo Ventures. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
