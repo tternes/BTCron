4e81d756604064e34e233d4b83770826a0c6a1bb
Thaddeus Ternes <thaddeus@bluetoo.co>
2015-05-23 23:33:48 -0500

    Add basic documentation for test category

--- BTCronTests/XCTestCase+CronTestBaseDates.h
+++ BTCronTests/XCTestCase+CronTestBaseDates.h
@@ -10,8 +10,23 @@
 
 @interface XCTestCase (CronTestBaseDates)
 
+/**
+ *  Base date for all tests - Thursday, January 1, 1970
+ *
+ *  @return Returns the base date for all tests (GMT)
+ */
 - (NSDate *)bt_baseDateForTests;
+
+/**
+ *  Base date timezone (GMT)
+ *
+ *  @return Returns the base date timezone (GMT)
+ */
 - (NSTimeZone *)bt_timezone;
+
+/**
+ *  Given an ISO string, returns the corresponding NSDate
+ */
 - (NSDate *)bt_dateFromIsoString:(NSString *)iso;
 
 @end
